6290021f0993d80d2676ec5cf70652910b637d4d
birdbird <me@birdbird.net>
2012-06-08 10:47:13 +0800

    update about slide

--- protected/views/page/about.php
+++ protected/views/page/about.php
@@ -9,14 +9,14 @@
 		<p>
 			<span>主 席：<a href="#">顾建平</a></span>
 			<span>副主席：<a href="#">姜志斌</a><a href="#">沈  飚</a>      
-			<span class="organization"><a href="<?php echo $this->createUrl('about',array('id'=>4))?>">常委会委员</a></span><span  class="organization"><a href="<?php echo $this->createUrl('about',array('id'=>5))?>">全委会委员</a></span>
+			<span class="organization"><a href="<?php echo $this->createUrl('about',array('id'=>5))?>">常委会委员</a></span><span  class="organization"><a href="<?php echo $this->createUrl('about',array('id'=>4))?>">全委会委员</a></span>
 			<span  class="organization"><a href="<?php echo $this->createUrl('about',array('id'=>1))?>">办公室</a></span><span  class="organization"><a href="<?php echo $this->createUrl('about',array('id'=>2))?>">普及部</a></span>
 			<span  class="organization"><a href="<?php echo $this->createUrl('about',array('id'=>3))?>">学会部</a></span>
 		</p>
-		<h3>历届主席</h3>
-		<p>
-			<span><a href="#">王高顺</a><a href="#">朱玉兰</a><a href="#">张  康</a><a href="#">秦素萍</a>
-			<a href="#">曹思平</a><a href="#">高  静</a><a href="#">仇洪泽</a><a href="#">陈治环</a>
+		<h3>历届主席</h3>                
+		<p class="zhuxi">
+			<span><a href="#">秦素萍</a><a href="#">张  康</a><a href="#">朱玉兰</a><a href="#">王高顺</a>
+			<a href="#">陈治环</a><a href="#">仇洪泽</a><a href="#">高  静</a><a href="#">曹思平</a>
 			<a href="#">顾建平</a></span>
 		</p>
 	</div>

54b20171c101033369dae903bd3f80999b1509ce
birdbird <me@birdbird.net>
2012-06-11 15:14:30 +0800

    fix admin post

--- kejijie/protected/modules/admin/views/post/_form.php
+++ kejijie/protected/modules/admin/views/post/_form.php
@@ -43,7 +43,6 @@
 	<div class="row">
 		<?php echo $form->labelEx($model,'category_id'); ?>
 		<?php echo $form->dropDownList($model,'category_id',CHtml::listData(Category::model()->findAll(), 'id', 'name')); ?>
-		<?php echo $form->hiddenField($model,'category_id') ?>
 		<?php echo $form->error($model,'category_id'); ?>
 	</div>
 	<div class="row">
